0746521a3ae79c610d18df0c82b3e7be767e4856
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-12-19 00:31:01 +0800

    desk

--- stack delux.cpp
+++ stack delux.cpp
@@ -92,7 +92,7 @@ int main()
     Stack S;
     int done = 0;
 
-
+printf("we must must win win win!win win win wait me back\n"); 
     scanf("%d", &N);
     S = CreateStack(N);
     while ( !done ) {
